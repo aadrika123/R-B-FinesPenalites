1f88f567f38f5b84196b51bbeb011cca83e23efc
ukm3423 <ukm.office3423@gmail.com>
2023-09-27 13:36:45 +0530

    penalty transaction

--- app/Models/PenaltyTransaction.php
+++ app/Models/PenaltyTransaction.php
@@ -33,8 +33,13 @@ class PenaltyTransaction extends Model
             'full_name',
             'challan_no',
             'challan_date',
+            'violations.violation_name',
+            'departments.department_name as department',
+
         )
             ->join('penalty_final_records', 'penalty_final_records.id', 'penalty_transactions.application_id')
-            ->join('penalty_challans', 'penalty_challans.id', 'penalty_transactions.challan_id');
+            ->join('penalty_challans', 'penalty_challans.id', 'penalty_transactions.challan_id')
+            ->join('violations', 'violations.id', 'penalty_final_records.violation_id')
+            ->join('departments', 'departments.id', 'violations.department_id');
     }
 }
