7eb9b57c24cddc70410a281e084d017729fff434
akshay2s <akshay.aadrika@gmail.com>
2024-02-02 13:52:34 +0530

    ,,,

--- app/Http/Controllers/Penalty/PaymentController.php
+++ app/Http/Controllers/Penalty/PaymentController.php
@@ -67,8 +67,8 @@ class PaymentController extends Controller
             if (!$challanDetails)
                 throw new Exception("Challan Not Found");
             $orderData = $api->order->create(array('amount' => $challanDetails->total_amount * 100, 'currency' => 'INR',));
-            $user  = authUser();
-            dd('a');
+            $user  = Auth()?authUser():null;
+            
             $mReqs = [
                 "order_id"       => $orderData['id'],
                 "merchant_id"    => $req->merchantId,
