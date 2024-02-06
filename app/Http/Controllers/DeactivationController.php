f53d5d600dbce8fb64aff0aa3ff463a93ae42e38
akshay2s <akshay.aadrika@gmail.com>
2024-02-06 14:08:59 +0530

    applicatiob Not found

--- app/Http/Controllers/DeactivationController.php
+++ app/Http/Controllers/DeactivationController.php
@@ -8,6 +8,7 @@ use App\Models\PenaltyRecord;
 use App\Models\PenaltyTransaction;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class DeactivationController extends Controller
@@ -28,7 +29,10 @@ class DeactivationController extends Controller
         if ($validator->fails())
             return validationError($validator);
         try {
+            DB::beginTransaction();
             $penaltyDtls = PenaltyFinalRecord::where('application_no', $request->applicationNo)->where('status', 1)->first();
+            if (!$penaltyDtls)
+                throw new Exception("Application Not Found");
             $penaltyDtls->status = 0;
             $penaltyDtls->save();
 
@@ -36,14 +40,22 @@ class DeactivationController extends Controller
             $penaltyChalan->status = 0;
             $penaltyChalan->save();
 
+            if ($penaltyChalan->payment_date) {
+                $tranDtls = PenaltyTransaction::where('challan_id', $penaltyChalan->id)->where('status', 1)->first();
+                $tranDtls->status = 0;
+                $tranDtls->save();
+            }
+
             if ($penaltyDtls->applied_record_id) {
                 $penaltyAppliedRecord = PenaltyRecord::find($penaltyDtls->applied_record_id);
                 $penaltyAppliedRecord->status = 0;
                 $penaltyAppliedRecord->save();
             }
 
+            DB::commit();
             return responseMsgs(true, "Application Deactivated", [], "1001", "01", responseTime(), $request->getMethod(), $request->deviceId);
         } catch (Exception $e) {
+            DB::rollBack();
             return responseMsgs(false, $e->getMessage(), [], "1001", "01", responseTime(), $request->getMethod(), $request->deviceId);
         }
     }
@@ -63,18 +75,29 @@ class DeactivationController extends Controller
         if ($validator->fails())
             return validationError($validator);
         try {
+            DB::beginTransaction();
             $challanDtls = PenaltyChallan::where('challan_no', $request->challanNo)->first();
+            if (!$challanDtls)
+                throw new Exception("Challan Not Found");
             $challanDtls->status = 0;
             $challanDtls->save();
 
-            if ($challanDtls->challan_type = 'On Spot') {
+            if ($challanDtls->payment_date) {
+                $tranDtls = PenaltyTransaction::where('challan_id', $challanDtls->id)->where('status', 1)->first();
+                $tranDtls->status = 0;
+                $tranDtls->save();
+            }
+
+            if ($challanDtls->challan_type == 'On Spot') {
                 $finalRecordDtls =  PenaltyFinalRecord::find($challanDtls->penalty_record_id);
                 $finalRecordDtls->status = 0;
                 $finalRecordDtls->save();
             }
 
+            DB::commit();
             return responseMsgs(true, "Challan Deactivated", [], "1002", "01", responseTime(), $request->getMethod(), $request->deviceId);
         } catch (Exception $e) {
+            DB::rollBack();
             return responseMsgs(false, $e->getMessage(), [], "1002", "01", responseTime(), $request->getMethod(), $request->deviceId);
         }
     }
@@ -92,8 +115,11 @@ class DeactivationController extends Controller
         if ($validator->fails())
             return validationError($validator);
         try {
+            DB::beginTransaction();
             $tranDtls = PenaltyTransaction::where('tran_no', $request->tranNo)
                 ->first();
+            if (!$tranDtls)
+                throw new Exception("No Transaction Found");
             $tranDtls->status = 0;
             $tranDtls->save();
 
@@ -105,8 +131,10 @@ class DeactivationController extends Controller
             $penaltyDtls->payment_status = false;
             $penaltyDtls->save();
 
+            DB::commit();
             return responseMsgs(true, "Payment Deactivated", [], "1003", "01", responseTime(), $request->getMethod(), $request->deviceId);
         } catch (Exception $e) {
+            DB::rollBack();
             return responseMsgs(false, $e->getMessage(), [], "1003", "01", responseTime(), $request->getMethod(), $request->deviceId);
         }
     }
