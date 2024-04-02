b22b58d73dbd8cb95d630f46a91836e5c69de1f7
mrinal9911 <mrinalk123456@gmail.com>
2024-04-02 15:40:50 +0530

    https://www.jharkhandegovernance.com

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://www.jharkhandegovernance.com'],
 
     'allowed_origins_patterns' => [],
 
