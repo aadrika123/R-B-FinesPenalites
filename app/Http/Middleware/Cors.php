f4f77c1422312a293700c61c23435aef63d47be6
mrinal9911 <mrinalk123456@gmail.com>
2024-04-02 16:09:44 +0530

    reponse

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,18 +16,11 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        // $response = $next($request);
+        $response = $next($request);
 
-        // // Set the Access-Control-Allow-Origin header to the request's origin
-        // $response->headers->set('Access-Control-Allow-Origin', $request->header('Origin'));
+        // Set the Access-Control-Allow-Origin header to the request's origin
+        $response->headers->set('Access-Control-Allow-Origin', $request->header('Origin'));
 
-        // return $response;
-
-        $origin = $request->header('Origin');
-
-        // Set the Access-Control-Allow-Origin header to the requesting origin
-        return response()
-            ->json(['data' => 'your_response_data'])
-            ->header('Access-Control-Allow-Origin', $origin);
+        return $response;
     }
 }
